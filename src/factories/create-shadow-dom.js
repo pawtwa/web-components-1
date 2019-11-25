c075fbee4516d7993c48532b15a1f846f07b9335
Paweł Twardziak <ptwardziak@eversis.com>
2019-11-26 00:32:21 +0100

    Use `slot.assignedElements()` & `slot.assignedNodes()` to inspect slot's distributed elements/nodes

--- src/factories/create-shadow-dom.js
+++ src/factories/create-shadow-dom.js
@@ -4,13 +4,30 @@ const createShadowDOM = (host, template) => {
 
     if (host instanceof HTMLElement) {
         console.log('`p` elements queried on Shadow DOM Host from Shadow DOM Host before its Shadow DOM root creating', host.querySelectorAll('p'));
+
         const root = host.attachShadow({mode: "closed"});
+
         console.log('shadowRoot mode `closed`: host.shadowRoot - ', host.shadowRoot, '; host.attachShadow return - ', root);
         console.log('`p` elements queried on Shadow DOM Host from Shadow DOM Host after Shadow DOM root creating', host.querySelectorAll('p'));
         console.log('`p` elements queried on Shadow DOM root from Shadow DOM after Shadow DOM root creating', root.querySelectorAll('p'));
+
         root.appendChild(document.importNode(template.content, true));
+
         console.log('`p` elements queried on Shadow DOM root from Shadow DOM root after appending its imported template with insertions', root.querySelectorAll('p'));
         console.log('`content[select^=p]` elements queried on Shadow DOM root from Shadow DOM root after appending its imported template with insertions', root.querySelectorAll('content[select^=p]'));
+
+        console.log(root.querySelectorAll('slot'));
+
+        console.log('shadow root slots nodes (flatten)', Array.prototype.reduce.call(root.querySelectorAll('slot'), (prev, slot) => {
+            prev.push(slot.assignedNodes({flatten: true}));
+            return prev;
+        }, []));
+
+        console.log('shadow root slots elements (flatten)', Array.prototype.reduce.call(root.querySelectorAll('slot'), (prev, slot) => {
+            prev.push(slot.assignedElements({flatten: true}));
+            return prev;
+        }, []));
+
         setTimeout(() => {
             console.log('%ccreateShadowDOM -> setTimeout:3000', afterTimerLogStyle);
             console.timeLog(timerName);
