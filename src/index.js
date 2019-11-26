e490f6106054e54076d0a94a6fabd495d218e6ec
Paweł Twardziak <ptwardziak@eversis.com>
2019-11-26 21:31:27 +0100

    Test applying style, script and html from imported html file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import "./resources/styles/main.scss";
 
-const htmlViewDocument = document.getElementById('first-view-html').import;
-const viewNode = htmlViewDocument.body.querySelector('#template-1');
-document.body.prepend(document.importNode(viewNode, true));
-
-console.log('thisViewDocument', thisViewDocument);
-
-const par = document.querySelector('p');
-par.style.padding = '20px';
-par.style.color = 'white';
+// const htmlViewDocument = document.getElementById('first-view-html').import;
+// const viewNode = htmlViewDocument.body.querySelector('#template-1');
+// document.body.prepend(document.importNode(viewNode, true));
+//
+// console.log('thisViewDocument', thisViewDocument);
+//
+// const par = document.querySelector('p');
+// par.style.padding = '20px';
+// par.style.color = 'white';
